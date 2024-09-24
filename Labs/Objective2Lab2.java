c95d010212291edaab517ec99df99383fcc0d04d
Leslie <leslie_aldaz@hotmail.com>
2024-09-23 22:50:48 -0600

    completed obj2 lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Leslie Wiseman";
+		int age = 30;
+		String state = "Oklahoma";
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(state);
+		System.out.println ("Name: "+ name + " Age: " + age + " State: " + state);
+	}
+}
