857998d4f5ea189aa17a8452f8369713b1c61033
Leslie <leslie_aldaz@hotmail.com>
2024-10-13 16:40:41 -0600

    Completed Obj7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,13 @@
+public class Objective7Lab4 {
+	public static void main(String[] args) {
+		int counter = 0;
+		int sum = 0;
+
+		do{ 
+			sum = sum + counter;
+			counter = counter + 1;
+        }
+		while(counter <= 20);
+	System.out.println(sum);
+}
+}
