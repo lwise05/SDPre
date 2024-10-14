c404f3518214efd9d7c58148228ddc6441be4b14
Leslie <leslie_aldaz@hotmail.com>
2024-10-13 18:21:12 -0600

    Completed Obj8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,11 @@
+public class Objective8Lab2 {
+	public static void main(String[] args) {
+		int sum = 0;
+		int i;
+
+		for(i=1; i<= 20; i++){
+			sum = sum + i;
+		}
+			System.out.println(sum);
+	}
+}
